23cb30a767c464325bc0e94255d62e325445ca31
garyerneus-oss <garyerneus@gmail.com>
2025-10-24 13:37:26 -0700

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MurMaxRideshareApp from "./MurmaxRideshareApp";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <MurMaxRideshareApp />
-  </React.StrictMode>
-);
